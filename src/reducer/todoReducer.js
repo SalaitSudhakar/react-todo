6f9680a0bdec6fa181edcb8076dce973394c9fbe
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2026-03-10 12:13:38 +0530

    Add CRUD operations of todoReducer

---
+++ src/reducer/todoReducer.js
@@ -0,0 +1,53 @@
+import ACTIONS from "./actionTypes";
+
+const { ADD, EDIT, REMOVE, TOGGLE } = ACTIONS;
+
+export default function todoReducer(state, action) {
+  const { todos, nextId } = state;
+  const { type, payload = "" } = action;
+
+  switch (type) {
+    case ADD:
+      return {
+        todos: [...todos, payload],
+        nextId: nextId + 1,
+      };
+
+    case REMOVE:
+      return {
+        ...state,
+        todos: todos.filter((todo) => todo.id !== payload),
+      };
+
+    case EDIT:
+      return {
+        ...state,
+        todos: todos.map((todo) =>
+          todo.id === payload.id
+            ? {
+                ...todo,
+                title: payload.title,
+                priority: payload.priority,
+                category: payload.category,
+              }
+            : todo,
+        ),
+      };
+
+    case TOGGLE:
+      return {
+        ...state,
+        todos: todos.map((todo) =>
+          todo.id === payload
+            ? {
+                ...todo,
+                completedStatus: !todo.completedStatus,
+              }
+            : todo,
+        ),
+      };
+
+    default:
+      return state;
+  }
+}
